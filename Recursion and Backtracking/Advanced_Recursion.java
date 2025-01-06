19085699f0b169dee198ffe4d835daed67448984
crazypunk08 <naren.lakamannavar@gmail.com>
2025-01-06 10:57:47 +0530

    Tiling problem

---
+++ Recursion and Backtracking/Advanced_Recursion.java
@@ -0,0 +1,14 @@
+public class Advanced_Recursion {
+    public static void main(String[] args) {
+        
+
+    }
+    
+    //Tilling Problem (Amazon) 
+        //Given a "2*n" floor and tiles of size "2*1" count the no of ways to tile the given floor using the 2*1 tiles
+        //(A tile can either be placed horizontally or vertically)
+    
+    public static int TP(int n){
+        return 0;
+    }
+}
