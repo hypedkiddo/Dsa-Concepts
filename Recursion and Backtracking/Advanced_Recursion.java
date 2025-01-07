27ebd1788cce4a25171f609544b82eb16948c998
crazypunk08 <naren.lakamannavar@gmail.com>
2025-01-07 10:01:19 +0530

    added problems

--- Recursion and Backtracking/Advanced_Recursion.java
+++ Recursion and Backtracking/Advanced_Recursion.java
@@ -1,6 +1,12 @@
 public class Advanced_Recursion {
     public static void main(String[] args) {
-        System.out.println(TP(10));
+        boolean map[]=new boolean[26];
+        String s="apnancollege";
+        String ans="";
+        int i=0;
+        rmdup(s, ans, i, map);
+
+
     }
     
     //Tilling Problem (Amazon) 
@@ -13,5 +19,30 @@ public class Advanced_Recursion {
         int n1=TP(n-1);
         int n2=TP(n-2);
         return n1+n2;
+    }
+
+       // Length of a String using Recursion
+    public static int len(String str, int i, int length) {
+        if (i == str.length()) {
+            return length;
+        }
+        return len(str, i + 1, length + 1);
+    }
+
+    //Remove Duplicates from  a String
+    public static  void  rmdup(String s,String ans,int i,boolean[] arr){
+        //base case
+        if(s.length()==i){
+            System.out.println(ans);
+            return;
+        }
+        //kaam
+        if(!arr[s.charAt(i)-'a']){
+            ans+=s.charAt(i);
+            arr[s.charAt(i)-'a']=true;
+        }
+        //Recursion
+         rmdup(s,ans,i+1,arr);
+
     }
 }
