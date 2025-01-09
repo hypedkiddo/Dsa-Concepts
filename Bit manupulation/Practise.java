530b821a58a18ce3c47fbf05b25abf2d876a1ab1
crazypunk08 <naren.lakamannavar@gmail.com>
2025-01-09 10:35:52 +0530

    added methods to clear bits and count set bits

--- Bit manupulation/Practise.java
+++ Bit manupulation/Practise.java
@@ -1,71 +1,112 @@
+
 import java.util.*;
+
 public class Practise {
+
     public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter your no");
-       int n=sc.nextInt();
+        int n = sc.nextInt();
     }
+
     //Function to check whether a number is odd or not
-    private static Boolean check(int n){
-        int ans=n&1;
-       if(ans!=0){
-           return true;//it is a odd number
-       }
-       return false;
+    private static Boolean check(int n) {
+        int ans = n & 1;
+        if (ans != 0) {
+            return true;//it is a odd number
+        }
+        return false;
     }
+
     //Get ith bit
-    private static int getbit(int n,int i){
-        int it=1 << i; //Left shift by i places
-        int ans=n & it;
-        if(ans==0){
+    private static int getbit(int n, int i) {
+        int it = 1 << i; //Left shift by i places
+        int ans = n & it;
+        if (ans == 0) {
             return 0;
         }
         return 1;
     }
+
     //Set the ith bit
-    private static  int setbit(int n,int i){
-        int it=1 << i;
-        int  ans= n | it;
+    private static int setbit(int n, int i) {
+        int it = 1 << i;
+        int ans = n | it;
         return ans;
     }
 
     //Clear the ith bit
-    private static int clearbit(int n,int i){
-        int it= ~(1 << i);
-        int  ans= n & it;
+    private static int clearbit(int n, int i) {
+        int it = ~(1 << i);
+        int ans = n & it;
         return ans;
     }
 
     //Update the ith bit
-    private static int updatebit(int n,int i,int newbit){
-        int it= ~(1 << i);
-        int  ans= n & it;
+    private static int updatebit(int n, int i, int newbit) {
+        int it = ~(1 << i);
+        int ans = n & it;
         return ans;
     }
 
-
-
-    //Function to convert a number to Binary
-    private static String toBinary(int n){
-        String ans="";
-        while(n>0){
-            if(n%2==0){
-                ans+="0";
-            }
-            else{
-                ans+="1";
-            }
-            n=n/2;
+    //clear last i bits
+    public static int clearLastbits(int n, int i) {
+        for (int j = 0; j < i; j++) {
+            int bitMask = ~(1 << j);
+            n = n & bitMask;
         }
-        String fans= reverse(ans);
-        return fans;
+        return n;
+    }
+
+    //clear range of bits
+    public static int clearBitsinrange(int n, int i, int j) {
+        //Brute approach
+        // for(int k=i;k<=j;k++){
+        //     int  bitMask=~(1<<k);
+        //     n=n&bitMask;
+        // }
+        // return n;
+
+        //optimized approach
+        int a = ((~0) << (j + 1));
+        int b = (1 << i) - 1;
+        int bitMask = a | b; //Here bitmask is achieved by two operations done simultaneously
+        n = n & bitMask;
+        return n;
+    }
+
+    //Count set bits in a number Important one 
+    public static int count(int n, int count) {
+        while (n != 0) {
+            int bitMask = 1;
+            if ((n & bitMask) == 1) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
     }
 
 
+    //Function to convert a number to Binary
+    private static String toBinary(int n) {
+        String ans = "";
+        while (n > 0) {
+            if (n % 2 == 0) {
+                ans += "0";
+            } else {
+                ans += "1";
+            }
+            n = n / 2;
+        }
+        String fans = reverse(ans);
+        return fans;
+    }
+
     private static String reverse(String ans) {
-        String fans="";
-        for(int i=ans.length()-1;i>=0;i--){
-            fans+=ans.charAt(i);
+        String fans = "";
+        for (int i = ans.length() - 1; i >= 0; i--) {
+            fans += ans.charAt(i);
         }
         return fans;
     }
